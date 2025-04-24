ff458fe34f80e6d44bdf98751a99088e575e848c
Alexis <abarreratrigo@gmail.com>
2025-04-24 20:04:00 +0200

    Para corregir el error, nos damos cuenta que al haber un alumno sin notas, nos devuelve un NaN en lugar de un 0. Modificamos un poco la estructura del return para además arreglar otro pequeño error existente en el código (si un alumno tiene solo dos notas, no se le hace la media entre 3, cuando se debería de contar la 3 nota como un 0)

--- src/GestorEstudiantes.java
+++ src/GestorEstudiantes.java
@@ -10,7 +10,9 @@ public class GestorEstudiantes {
         for (int i = 0; i < estudiante.getNotas().length; i++) { // Error: índice fuera de rango
             suma += estudiante.getNotas()[i];
         }
-        return suma / estudiante.getNotas().length; // Error si el array está vacío
+        if (estudiante.getNotas() != null){
+            return suma/3; // Error si el array está vacío
+        }else return 0;
     }
 
     // Encuentra al estudiante con la mejor nota media
